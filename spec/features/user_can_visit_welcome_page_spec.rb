8a3d4ff3de13326fd16f20e837c86d09af4b8b5e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-26 08:39:02 -0700

    Initial commit

--- spec/features/user_can_visit_welcome_page_spec.rb
+++ spec/features/user_can_visit_welcome_page_spec.rb
@@ -7,4 +7,26 @@ describe "user can visit the welcome page" do
     expect(page).to have_content("Search For The Nearest Electric Charging Station")
     expect(page).to have_button("Find Nearest Station")
   end
+
+  scenario "select Turing from drop down and click Find Nearest Station" do
+    visit '/'
+
+    select "Turing" #from start location drop down
+    click_on "Find Nearest Station"
+
+    expect(current_path).to eq("/search")
+  end
 end
+
+# Then I should see the closest electric fuel station to me.
+# For that station I should see
+# - Name
+# - Address
+# - Fuel Type
+# - Access Times
+#
+# I should also see:
+# - the distance of the nearest station (should be 0.1 miles)
+# - the travel time from Turing to that fuel station (should be 1 min)
+# - The direction instructions to get to that fuel station
+#   "Turn left onto Lawrence St Destination will be on the left"
