5f3994087ead905d565c321c3afd0655de768063
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-27 09:37:23 -0700

    Add notes for test

--- spec/features/user_can_visit_welcome_page_spec.rb
+++ spec/features/user_can_visit_welcome_page_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "user can visit the welcome page" do
+describe "user can visit the welcome page", type: :view do
   scenario "and see a dropdown menu" do
     visit '/'
     expect(page).to have_content("Nearest Fuel Station")
@@ -11,20 +11,20 @@ describe "user can visit the welcome page" do
   scenario "select Turing from drop down and click Find Nearest Station" do
     visit '/'
 
-    select "Turing" #from start location drop down
+    select "Turing"
+# ["Turing", "1331 17th St, Denver, CO 80202"]
     click_on "Find Nearest Station"
 
     expect(current_path).to eq("/search")
+    # Then I should see the closest electric fuel station to me.
+    # For that station I should see
+    # expect(page).to have_content(station.name)
+    # expect(page).to have_content(station.address)
+    # expect(page).to have_content(station.fuel_type)
+    # expect(page).to have_content(station.access_times)
   end
 end
 
-# Then I should see the closest electric fuel station to me.
-# For that station I should see
-# - Name
-# - Address
-# - Fuel Type
-# - Access Times
-#
 # I should also see:
 # - the distance of the nearest station (should be 0.1 miles)
 # - the travel time from Turing to that fuel station (should be 1 min)
